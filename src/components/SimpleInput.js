080007419d90331cd422851eaeb22df88bd9ebe1
barry <mbrsyr@yahoo.com>
2023-01-11 18:11:34 +0100

    gathering form data

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,36 @@
+import { useRef, useState } from 'react';
+
 const SimpleInput = (props) => {
+  const nameInput = useRef();
+  const [enteredName, setEnteredName] = useState();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    const enteredValue = nameInput.current.value;
+    console.log(enteredValue);
+
+    nameInput.current.value = '';
+    setEnteredName('');
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          type='text'
+          id='name'
+          onChange={nameInputChangeHandler}
+          ref={nameInput}
+          value={enteredName}
+        />
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
